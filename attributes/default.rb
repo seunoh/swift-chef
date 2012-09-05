74b1640c3e3f455d4e42029106912a53a6e24cc5
root <root@chefserver.(none)>
2012-09-06 05:59:00 +0900

    swift-init

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+default[:cloudfiles][:user] = "root"
+default[:cloudfiles][:group] = "root"
+default[:cloudfiles][:homedir] = "/root"
+default[:cloudfiles][:bzr_email] = "you@example.com"
+default[:cloudfiles][:bzr_branch] = "lp:swift"
+default[:cloudfiles][:launchpad_login] = "gimme"
+default[:cloudfiles][:mount_path] = "/srv/node"
+default[:cloudfiles][:mounted_drives] = "7"
